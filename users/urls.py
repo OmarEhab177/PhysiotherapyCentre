d84ededdc86889f13a0246055979acae254b1599
Omar Ehab <56652848+OmarEhab177@users.noreply.github.com>
2022-07-13 23:46:26 +0200

    not finished yet (#10)

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
 
-from .views import UserView, signup
+from .views import UserView, signup, login_view
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -10,7 +10,8 @@ from django.conf.urls.static import static
 app_name = 'users'
 
 urlpatterns = [
-    path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    # path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login', login_view, name='login'),
     path('logout', auth_views.LogoutView.as_view(next_page='/accounts/login'), name='logout'),
     path('profile/', login_required(UserView.as_view()), name='profile'),
     path('signup', signup, name='signup')
--- users/views.py
+++ users/views.py
@@ -1,9 +1,14 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.views.generic.detail import DetailView
-
+from django.contrib.auth.forms import (
+    AuthenticationForm, PasswordChangeForm, PasswordResetForm, SetPasswordForm,
+)
+from django.contrib import messages
 from .forms import SignUpForm
 
+from django.contrib import sessions
 
 class UserView(DetailView):
     template_name = 'users/profile.html'
@@ -17,7 +22,6 @@ def signup(request):
         form = SignUpForm(request.POST)
         print(form)
         if form.is_valid():
-            print(form.cleaned_data)
             user = form.save()
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(request, email=user.email, password=raw_password)
@@ -29,3 +33,22 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'users/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user and user.is_active:
+            login(request, user)
+            if user.groups.filter(name = 'therapist').exists():
+                request.session['group'] = 'therapist'
+                return HttpResponseRedirect('/therapists')
+            else:
+                request.session['group'] = user.groups.first()
+        else:
+            messages.add_message(request, messages.INFO, 'Please enter a correct email and password. Note that, May be your account is disabled please contact us')
+        return HttpResponseRedirect('/')
+    else:
+        form = AuthenticationForm()
+        return render(request, 'users/login.html', {'form':form})
